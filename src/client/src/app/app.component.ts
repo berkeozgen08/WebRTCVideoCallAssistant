bad73f804a61ec325dd51bf792abf1dd6c9380eb
Barış Can Yılmaz <baris@bariscanyilmaz.com>
2022-03-13 23:35:46 +0300

    feat:render received data

--- src/client/src/app/app.component.ts
+++ src/client/src/app/app.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { filter, Observable, of } from 'rxjs';
+import { CallService } from './services/call.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy, AfterViewInit, OnInit {
+
   title = 'WebRTCVideoCallAssistant.Client';
+
+  @ViewChild('localVideo') localVideo: ElementRef<HTMLVideoElement>;
+  @ViewChild('remoteVideo') remoteVideo: ElementRef<HTMLVideoElement>;
+
+  targetID: string = "";
+  peerID: string;
+  public isCallStarted$: Observable<boolean>;
+
+
+  /**
+   *
+   */
+  constructor(private callService: CallService) {
+  }
+
+  ngOnInit(): void {
+    this.isCallStarted$ = this.callService.isCallStarted$;
+    this.peerID = this.callService.initPeer();
+  }
+
+  ngAfterViewInit(): void {
+    this.callService.localStream$
+      .pipe(filter(res => !!res))
+      .subscribe(stream => {
+        this.localVideo.nativeElement.srcObject = stream
+      });
+
+    this.callService.remoteStream$
+      .pipe(filter(res => !!res))
+      .subscribe(stream => {
+        this.remoteVideo.nativeElement.srcObject = stream
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.callService.destroyPeer();
+  }
+
+  showModal(join: boolean) {
+    of(join ? this.callService.establishMediaCall(this.targetID) : this.callService.enableCallAnswer()).subscribe(_ => { });
+  }
+
+  public endCall() {
+    this.callService.closeMediaCall();
+  }
+
 }
