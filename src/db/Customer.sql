efd7ff4ca04c1f859b9d5d56e4a5a1aef032c675
Berke Özgen <berkeozgen08@hotmail.com>
2022-04-30 23:41:57 +0300

    feat: implementing db

--- src/db/Customer.sql
+++ src/db/Customer.sql
@@ -1,7 +1,8 @@
 CREATE TABLE [dbo].[Customer]
 (
-	[Id] INT NOT NULL PRIMARY KEY,
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1, 1),
 	[FirstName] NVARCHAR(50) NOT NULL,
 	[LastName] NVARCHAR(50) NOT NULL,
-	CHECK ([FirstName] <> '' AND [LastName] <> '')
+	CHECK ([FirstName] <> '' AND [LastName] <> ''),
+	UNIQUE ([FirstName], [LastName])
 )
---
+++ src/db/Meeting.sql
@@ -0,0 +1,12 @@
+CREATE TABLE [dbo].[Meeting]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1, 1),
+	[CreatedBy] INT NOT NULL REFERENCES [dbo].[User](Id),
+	[CreatedFor] INT NOT NULL REFERENCES [dbo].[Customer](Id),
+	[UserConnId] CHAR(36) NOT NULL,
+	[CustomerConnId] CHAR(36) NOT NULL,
+	[UserSlug] CHAR(10) NOT NULL,
+	[CustomerSlug] CHAR(10) NOT NULL,
+	[StartsAt] DATETIME NOT NULL
+	[CreatedAt] DATETIME NOT NULL DEFAULT GETDATE(),
+)
