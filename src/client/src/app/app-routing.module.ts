3a3ddd5469964be590b143fa7bccc9505155487f
Barış Can Yılmaz <baris@bariscanyilmaz.com>
2022-05-22 12:04:38 +0300

    add components to routing

--- src/client/src/app/app-routing.module.ts
+++ src/client/src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { MeetingStatsComponent } from './components/meeting-stats/meeting-stats.component';
+import { MeetingComponent } from './components/meeting/meeting.component';
+import { SetMeetingComponent } from './components/set-meeting/set-meeting.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path:'',
+  component:HomeComponent
+},{
+  path:'meeting',
+  component:MeetingComponent
+},{
+  path:'meeting-stats',
+  component:MeetingStatsComponent
+},{
+  path:'set-meeting',
+  component:SetMeetingComponent
+},{
+  path:'**',
+  redirectTo:''
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
