5e65169bc97e8a86660c707dd51d0139be6938c7
Barış Can Yılmaz <baris@bariscanyilmaz.com>
2022-05-29 00:50:53 +0300

    implement meeting crud service

--- src/client/src/app/components/set-meeting/set-meeting.component.ts
+++ src/client/src/app/components/set-meeting/set-meeting.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Customer } from 'src/app/models/customer';
+import { Meeting } from 'src/app/models/meeting';
+import { User } from 'src/app/models/user';
+import { CustomerService } from 'src/app/services/customer.service';
+import { MeetingService } from 'src/app/services/meeting.service';
+import { UserService } from 'src/app/services/user.service';
 
 
 @Component({
@@ -8,20 +15,66 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./set-meeting.component.scss']
 })
 export class SetMeetingComponent implements OnInit {
-  
-  meetingId:number
 
-  constructor(private route:ActivatedRoute) { }
+  meetingId: number
+  isNewRecord: boolean;
+  meeting: Meeting={
+    id:0,
+    createdAt:null,
+    createdBy:null,
+    createdFor:null,
+    createdByNavigation:null,
+    createdForNavigation:null,
+    customerSlug:null,
+    startsAt:null,
+    userSlug:null,
+    statId:null,
+    stat:null
+  };
+
+  users:User[];
+  customers:Customer[];
+
+  constructor(
+    private route: ActivatedRoute, 
+    private meetingService: MeetingService, 
+    private customerService:CustomerService,
+    private userService:UserService,
+    private toastService: ToastrService) { }
 
   ngOnInit(): void {
-    
-    this.route.queryParams.subscribe({
-      next:(params)=>{
-        this.meetingId=(params['meetingId'])
-        
-      }
+    let id = +this.route.snapshot.paramMap.get("id");
+    this.isNewRecord = !(!!id);
+
+    this.customerService.getAll().subscribe({
+      next:(v)=>this.customers=v
     });
-    
+
+    this.userService.getAll().subscribe({
+      next:(v)=>this.users=v
+    });
+
+    if (!this.isNewRecord) {
+      this.meetingService.get(id).subscribe({
+        next: (v) => this.meeting = v
+      });
+    }
   }
 
+  onSubmit(): void {
+    if (this.isNewRecord) {
+      this.meetingService.create(this.meeting).subscribe({
+        next: (v) => this.toastService.success(`Meeting created successfully.`),
+        error: (err) => this.toastService.error(err)
+      })
+
+    } else {
+      this.meetingService.update(this.meeting).subscribe({
+        next: (v) => this.toastService.success(`Meeting updated successfully.`),
+        error: (err) => this.toastService.error(err)
+      });
+    }
+  }
+
+  
 }
---
+++ src/client/src/app/services/meeting.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Meeting, MeetingCreate } from '../models/meeting';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MeetingService {
+
+  constructor(private http: HttpClient) { }
+
+  public get(id: number) {
+    return this.http.get<Meeting>(`${environment.API_URL}Meeting/Get/${id}`);
+  }
+
+  public getAll() {
+    return this.http.get<Meeting[]>(`${environment.API_URL}Meeting/GetAll`);
+  }
+
+  public create(meeting: MeetingCreate) {
+    return this.http.put<Meeting>(`${environment.API_URL}Meeting/Create`, meeting);
+  }
+
+  public update(meeting: Meeting) {
+    //let update: MeetingUpdate = { password: meeting.password, phone: Meeting.phone }
+    return this.http.patch(`${environment.API_URL}Meeting/Update/${meeting.id}`, meeting);//we Meeting in body id unnecessary
+  }
+
+  public delete(id: number) {
+    return this.http.delete<Meeting>(`${environment.API_URL}Meeting/Delete/${id}`);
+  }
+}
