cc05d31458952404b05ed1e7c6d7154e664df120
Berke Özgen <berkeozgen08@hotmail.com>
2022-06-03 16:04:12 +0300

    finalize meeting controls

--- src/client/src/app/components/meeting/meeting.component.ts
+++ src/client/src/app/components/meeting/meeting.component.ts
@@ -1,117 +1,134 @@
 import { AfterViewInit, ChangeDetectorRef, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { filter, Observable, of } from 'rxjs';
+import { Customer } from "src/app/models/customer";
+import { Meeting } from "src/app/models/meeting";
+import { User } from "src/app/models/user";
 import { CallService } from 'src/app/services/call.service';
 import { MeetingService } from "src/app/services/meeting.service";
 
 @Component({
-  selector: 'app-meeting',
-  templateUrl: './meeting.component.html',
-  styleUrls: ['./meeting.component.scss']
+	selector: 'app-meeting',
+	templateUrl: './meeting.component.html',
+	styleUrls: ['./meeting.component.scss']
 })
 export class MeetingComponent implements OnDestroy, AfterViewInit, OnInit {
 
-  title = 'WebRTCVideoCallAssistant.Client';
+	title = 'WebRTCVideoCallAssistant.Client';
 
-  @ViewChild('localVideo') localVideo: ElementRef<HTMLVideoElement>;
-  @ViewChild('remoteVideo') remoteVideo: ElementRef<HTMLVideoElement>;
+	@ViewChild('localVideo') localVideo: ElementRef<HTMLVideoElement>;
+	@ViewChild('remoteVideo') remoteVideo: ElementRef<HTMLVideoElement>;
 
-  targetID: string = "";
-  peerID: string;
-  public isCallStarted$: Observable<boolean>;
-  isUser:boolean;
+	targetID: string = "";
+	peerID: string;
+	public isCallStarted$: Observable<boolean>;
+	isUser: boolean;
 
-  isMicOpen=true;
-  isLocalCamOpen=true;
-  isRemoteCamOpen=true;
+	isLocalMicOpen = true;
+	isLocalCamOpen = true;
+	isRemoteMicOpen = true;
+	isRemoteCamOpen = true;
+	meeting: Meeting;
+	local: User | Customer;
+	remote: User | Customer;
+	isloading: boolean = false;
 
-  /**
-   *
-   */
-  constructor(private callService: CallService,private route:ActivatedRoute, private meetingService: MeetingService, private changeDetector: ChangeDetectorRef) {
-  }
+	/**
+	 *
+	 */
+	constructor(private callService: CallService, private route: ActivatedRoute, private meetingService: MeetingService, private changeDetector: ChangeDetectorRef) {
+	}
 
-  ngOnInit(): void {
-	const slug = this.route.snapshot.paramMap.get("slug");
-    this.isCallStarted$ = this.callService.isCallStarted$;
-    this.route.queryParams.subscribe({
-      next:(params)=>{
-		// TODO:
-        // this.isUser = jwt.role == "user";
-        this.isUser = params["a"] == "u";
-		
-		this.meetingService.resolveSlug(slug).subscribe({
-			next: ({ userConnId, customerConnId }) => {
-				if (this.isUser) {
-					this.peerID = this.callService.initPeer(userConnId);
-					of(this.callService.enableCallAnswer()).subscribe();
-				} else {
-					this.peerID = this.callService.initPeer(customerConnId);
-					of(this.callService.establishMediaCall(userConnId)).subscribe();
-				}
-			},
-			error: console.error
+	ngOnInit(): void {
+		this.isloading = true;
+		const slug = this.route.snapshot.paramMap.get("slug");
+		this.isCallStarted$ = this.callService.isCallStarted$;
+		this.route.queryParams.subscribe({
+			next: (params) => {
+				// TODO:
+				// this.isUser = jwt.role == "user";
+				this.isUser = params["a"] == "u";
+
+				this.meetingService.resolveSlug(slug).subscribe({
+					next: (meeting) => {
+						this.meeting = meeting;
+						this.local = this.isUser ? meeting.createdBy : meeting.createdFor;
+						this.remote = this.isUser ? meeting.createdFor : meeting.createdBy;
+						const { userConnId, customerConnId } = meeting;
+						if (this.isUser) {
+							this.peerID = this.callService.initPeer(userConnId);
+							of(this.callService.enableCallAnswer()).subscribe();
+						} else {
+							this.peerID = this.callService.initPeer(customerConnId);
+							of(this.callService.establishMediaCall(userConnId)).subscribe();
+						}
+						this.callService.isConnectionStarted$.subscribe(() => {
+							this.callService.connection$.subscribe(conn => {
+								conn.on("data", ({ event, status }) => {
+									if (event === "video") {
+										this.isRemoteCamOpen = status;
+									} else if (event === "audio") {
+										this.isRemoteMicOpen = status;
+									}
+									console.log(event, status);
+									this.changeDetector.detectChanges();
+									if (event === "video" && status === true) {
+										this.callService.remoteStream$.subscribe(stream => {
+											this.remoteVideo.nativeElement.srcObject = stream;
+										});
+									}
+								});
+							});
+						});
+						this.isloading = false;
+					},
+					error: console.error
+				});
+			}
 		});
-      }
-    });
+	}
 
-    
-  }
+	ngAfterViewInit(): void {
+		this.callService.localStream$.subscribe(stream => {
+			if (!!stream) {
+				this.localVideo.nativeElement.srcObject = stream
+			}
+		});
 
-  ngAfterViewInit(): void {
-    this.callService.localStream$
-      .subscribe(stream => {
-        if(!!stream){
-          //this.isLocalCamOpen=true;
-          this.localVideo.nativeElement.srcObject = stream
-        }else{
-          //this.isLocalCamOpen=false;
-        }
+		this.callService.remoteStream$.subscribe(stream => {
+			if (!!stream) {
+				this.remoteVideo.nativeElement.srcObject = stream;
+			}
+		});
+	}
 
-        
-        
-      });
+	ngOnDestroy(): void {
+		this.callService.destroyPeer();
+	}
 
-    this.callService.remoteStream$
-      .subscribe(stream => {
-        if(!!stream){
-          //this.isRemoteCamOpen=true;
+	showModal(join: boolean) {
+		of(join ? this.callService.establishMediaCall(this.targetID) : this.callService.enableCallAnswer()).subscribe(_ => { });
+	}
 
-          this.remoteVideo.nativeElement.srcObject = stream;
-        }else{
-          //this.isRemoteCamOpen=false;
-        }
+	public endCall() {
+		this.callService.closeMediaCall();
+	}
 
-        
-      });
-  }
+	toggleVideo() {
+		this.isLocalCamOpen = !this.isLocalCamOpen;
+		this.callService.localStream$.subscribe((stream) => {
+			stream.getVideoTracks()[0].enabled = this.isLocalCamOpen;
+			if (this.isLocalCamOpen) {
+				this.changeDetector.detectChanges();
+				this.localVideo.nativeElement.srcObject = stream;
+			}
+			this.callService.connection$.subscribe(conn => conn.send({ event: "video", status: this.isLocalCamOpen }));
+		});
+	}
 
-  ngOnDestroy(): void {
-    this.callService.destroyPeer();
-  }
-
-  showModal(join: boolean) {
-    of(join ? this.callService.establishMediaCall(this.targetID) : this.callService.enableCallAnswer()).subscribe(_ => { });
-  }
-
-  public endCall() {
-    this.callService.closeMediaCall();
-  }
-
-  toggleVideo(){
-    this.isLocalCamOpen = !this.isLocalCamOpen;
-    this.callService.localStream$.subscribe((stream) => {
-      stream.getVideoTracks()[0].enabled = this.isLocalCamOpen;
-      if (this.isLocalCamOpen) {
-        this.changeDetector.detectChanges();
-        this.localVideo.nativeElement.srcObject = stream;
-      }
-    });
-  }
-  
-  toggleMicrophone(){
-    this.isMicOpen = !this.isMicOpen;
-    this.callService.localStream$.subscribe((stream) => stream.getAudioTracks()[0].enabled = this.isMicOpen);
-  }
-
+	toggleMicrophone() {
+		this.isLocalMicOpen = !this.isLocalMicOpen;
+		this.callService.localStream$.subscribe((stream) => stream.getAudioTracks()[0].enabled = this.isLocalMicOpen);
+		this.callService.connection$.subscribe(conn => conn.send({ event: "audio", status: this.isLocalMicOpen }));
+	}
 }
--- src/client/src/app/services/call.service.ts
+++ src/client/src/app/services/call.service.ts
@@ -1,163 +1,179 @@
 import { Injectable } from '@angular/core';
-import Peer from 'peerjs';
+import Peer, { DataConnection } from 'peerjs';
 import { BehaviorSubject, filter, Observable, Subject } from 'rxjs';
 import { v4 as uuidv4 } from 'uuid';
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class CallService {
 
-  constructor() { }
+	constructor() { }
 
-  private peer: Peer;
-  private statsInterval;
+	private peer: Peer;
+	private statsInterval;
 
-  private mediaCall: Peer.MediaConnection;
+	private mediaCall: Peer.MediaConnection;
 
-  private localStreamBs = new BehaviorSubject<MediaStream>(null);
+	private localStreamBs = new BehaviorSubject<MediaStream>(null);
+	public localStream$: Observable<MediaStream> = this.localStreamBs.asObservable();
+	private remoteStreamBs = new BehaviorSubject<MediaStream>(null);
+	public remoteStream$: Observable<MediaStream> = this.remoteStreamBs.asObservable();
 
-  public localStream$: Observable<MediaStream> = this.localStreamBs.asObservable();
-  private remoteStreamBs = new BehaviorSubject<MediaStream>(null);
-  public remoteStream$: Observable<MediaStream> = this.remoteStreamBs.asObservable();
+	private isCallStartedBs = new Subject<boolean>();
+	public isCallStarted$ = this.isCallStartedBs.asObservable();
 
-  private isCallStartedBs = new Subject<boolean>();
-  public isCallStarted$ = this.isCallStartedBs.asObservable();
+	public localId: string;
+	public remoteId: string;
 
-  public getUserMedia: ({ video, audio }: { video?: boolean, audio?: boolean }) => Promise<MediaStream> =
-    navigator.mediaDevices.getUserMedia ||
-    (navigator as any).mediaDevices.webkitGetUserMedia ||
-    (navigator as any).mediaDevices.mozGetUserMedia ||
-    (navigator as any).getUserMedia ||
-    (navigator as any).webkitGetUserMedia ||
-    (navigator as any).mozGetUserMedia
-  
-  initPeer(id:string): string {
+	public connectionBs = new BehaviorSubject<DataConnection>(null);
+	public connection$ = this.connectionBs.asObservable();
+	public isConnectionStartedBs = new Subject<boolean>();
+	public isConnectionStarted$ = this.isConnectionStartedBs.asObservable();
 
-    if (!this.peer || !this.peer.disconnected) {
+	public getUserMedia: ({ video, audio }: { video?: boolean, audio?: boolean }) => Promise<MediaStream> =
+		navigator.mediaDevices.getUserMedia ||
+		(navigator as any).mediaDevices.webkitGetUserMedia ||
+		(navigator as any).mediaDevices.mozGetUserMedia ||
+		(navigator as any).getUserMedia ||
+		(navigator as any).webkitGetUserMedia ||
+		(navigator as any).mozGetUserMedia
 
-      const peerJsOptions: Peer.PeerJSOption = {
-        debug: 3,
-        config: {
-          iceServers: [{
-            urls: [
-              'stun:stun1.l.google.com:19302',
-              'stun:stun2.l.google.com:19302',
-            ]
-          }]
-        }
-      };
+	initPeer(id: string): string {
+		if (!this.peer || !this.peer.disconnected) {
+			const peerJsOptions: Peer.PeerJSOption = {
+				debug: 3,
+				config: {
+					iceServers: [{
+						urls: [
+							'stun:stun1.l.google.com:19302',
+							'stun:stun2.l.google.com:19302',
+						]
+					}]
+				}
+			};
 
-      try {
-        //let id = uuidv4();
-        this.peer = new Peer(id, peerJsOptions);
-        return id;
-      } catch (error) {
-        console.error(error);
-      }
-    }
-
-    return "";
-  }
-
-  public async establishMediaCall(remotePeerId: string) {
-    
-    console.log(remotePeerId);
-
-    try {
-      const stream = await this.getUserMedia({ video: true, audio: true });
-
-      const connection = this.peer.connect(remotePeerId);
-      connection.on('error', err => {
-        console.error(err);
-        //this.snackBar.open(err, 'Close');
-      });
-
-      this.mediaCall = this.peer.call(remotePeerId, stream);
-      if (!this.mediaCall) {
-        let errorMessage = 'Unable to connect to remote peer';
-        //this.snackBar.open(errorMessage, 'Close');
-        throw new Error(errorMessage);
-      }
-      this.localStreamBs.next(stream);
-      this.isCallStartedBs.next(true);
-
-      this.mediaCall.on('stream',
-        (remoteStream) => {
-          this.remoteStreamBs.next(remoteStream);
-        });
-      this.mediaCall.on('error', err => {
-        //this.snackBar.open(err, 'Close');
-        console.error(err);
-        this.isCallStartedBs.next(false);
-      });
-      this.mediaCall.on('close', () => this.onCallClose());
-    }
-    catch (ex) {
-      console.error(ex);
-      //this.snackBar.open(ex, 'Close');
-      this.isCallStartedBs.next(false);
-    }
-  }
-
-  public async enableCallAnswer() {
-    try {
-      const stream = await this.getUserMedia({ video: true, audio: true });
-      this.localStreamBs.next(stream);
-      this.peer.on('call', async (call) => {
-        this.mediaCall = call;
-        this.isCallStartedBs.next(true);
-        this.statsInterval = setInterval(async () => await this.connectionStats(call.peerConnection), 1000);
-
-        this.mediaCall.answer(stream);
-        this.mediaCall.on('stream', (remoteStream) => {
-          this.remoteStreamBs.next(remoteStream);
-        });
-        this.mediaCall.on('error', err => {
-          //this.snackBar.open(err, 'Close');
-          this.isCallStartedBs.next(false);
-          console.error(err);
-        });
-        this.mediaCall.on('close', () => this.onCallClose());
-      });
-    }
-    catch (ex) {
-      console.error(ex);
-      //this.snackBar.open(ex, 'Close');
-      this.isCallStartedBs.next(false);
-    }
-  }
-
-  private onCallClose() {
-    this.remoteStreamBs.value!.getTracks().forEach(track => {
-      track.stop();
-    });
-    this.localStreamBs.value!.getTracks().forEach(track => {
-      track.stop();
-    });
-    //this.snackBar.open('Call Ended', 'Close');
-  }
-
-  public async connectionStats(conn: RTCPeerConnection) {
-	const stats = await conn.getStats(null);
-	stats.forEach(report => {
-		if (report.type === "inbound-rtp" && (report.kind === "video" || report.kind === "audio")) {
-			console.log(report);
+			try {
+				this.localId = id;
+				this.peer = new Peer(id, peerJsOptions);
+				return id;
+			} catch (error) {
+				console.error(error);
+			}
 		}
-	});
-  }
 
-  public closeMediaCall() {
-    this.mediaCall?.close();
-    if (!this.mediaCall) {
-      this.onCallClose()
-    }
-    this.isCallStartedBs.next(false);
-  }
+		return "";
+	}
 
-  public destroyPeer() {
-    this.mediaCall?.close();
-    this.peer?.disconnect();
-    this.peer?.destroy();
-  }
+	public async establishMediaCall(remotePeerId: string) {
+		this.remoteId = remotePeerId;
+		console.log(remotePeerId);
+		try {
+			const stream = await this.getUserMedia({ video: true, audio: true });
 
+			this.connectionBs.next(this.peer.connect(remotePeerId));
+			this.connectionBs.subscribe({
+				next: (conn) => {
+					conn.on('error', err => {
+						console.error(err);
+						//this.snackBar.open(err, 'Close');
+					});
+					this.isConnectionStartedBs.next(true);
+				}
+			});
+
+			this.mediaCall = this.peer.call(remotePeerId, stream);
+			if (!this.mediaCall) {
+				let errorMessage = 'Unable to connect to remote peer';
+				//this.snackBar.open(errorMessage, 'Close');
+				throw new Error(errorMessage);
+			}
+			this.localStreamBs.next(stream);
+			this.isCallStartedBs.next(true);
+
+			this.mediaCall.on('stream', (remoteStream) => {
+				this.remoteStreamBs.next(remoteStream);
+			});
+			this.mediaCall.on('error', (err) => {
+				//this.snackBar.open(err, 'Close');
+				console.error(err);
+				this.isCallStartedBs.next(false);
+			});
+			this.mediaCall.on('close', () => this.onCallClose());
+		}
+		catch (ex) {
+			console.error(ex);
+			//this.snackBar.open(ex, 'Close');
+			this.isCallStartedBs.next(false);
+		}
+	}
+
+	public async enableCallAnswer() {
+		try {
+			const stream = await this.getUserMedia({ video: true, audio: true });
+			this.localStreamBs.next(stream);
+			this.peer.on("connection", (conn) => {
+				this.connectionBs.next(conn);
+				this.isConnectionStartedBs.next(true);
+			});
+			this.peer.on('call', async (call) => {
+				this.mediaCall = call;
+				this.isCallStartedBs.next(true);
+				this.statsInterval = setInterval(async () => await this.connectionStats(call.peerConnection), 1000);
+
+				this.mediaCall.answer(stream);
+				this.remoteId = this.mediaCall.peer;
+				this.mediaCall.on('stream', (remoteStream) => {
+					this.remoteStreamBs.next(remoteStream);
+				});
+				this.mediaCall.on('error', err => {
+					//this.snackBar.open(err, 'Close');
+					this.isCallStartedBs.next(false);
+					console.error(err);
+				});
+				this.mediaCall.on('close', () => {
+					this.onCallClose()
+					clearInterval(this.statsInterval);
+				});
+			});
+		}
+		catch (ex) {
+			console.error(ex);
+			//this.snackBar.open(ex, 'Close');
+			this.isCallStartedBs.next(false);
+		}
+	}
+
+	private onCallClose() {
+		this.remoteStreamBs.value!.getTracks().forEach(track => {
+			track.stop();
+		});
+		this.localStreamBs.value!.getTracks().forEach(track => {
+			track.stop();
+		});
+		//this.snackBar.open('Call Ended', 'Close');
+		clearInterval(this.statsInterval);
+	}
+
+	public async connectionStats(conn: RTCPeerConnection) {
+		const stats = await conn.getStats(null);
+		stats.forEach(report => {
+			if (report.type === "inbound-rtp" && (report.kind === "video" || report.kind === "audio")) {
+				console.log(report);
+			}
+		});
+	}
+
+	public closeMediaCall() {
+		this.mediaCall?.close();
+		if (!this.mediaCall) {
+			this.onCallClose()
+		}
+		this.isCallStartedBs.next(false);
+	}
+
+	public destroyPeer() {
+		this.mediaCall?.close();
+		this.peer?.disconnect();
+		this.peer?.destroy();
+	}
 }
